e1284ac550df07830f1b26a044aa7f3adeca3ea4
randompast <matteroflight@gmail.com>
2018-03-31 23:16:39 -0400

    added orb 1-3 and pagoda 1-3

---
+++ 2018-3-31_SwampCTF/orb2.py
@@ -0,0 +1,13 @@
+# I made this challenge, official solution will be included when the swampctf repo gets posted
+
+# This was a hint in the example package, it had to do with the final image size
+# (500, 50)
+import math
+(x,y,v,u,th) =(-804.1791798191063, 207.81139231370207, 100.82602527985537, 1.080075821502287, 2.9241898277603076)
+g = 9.81
+
+t = 2.0 * v * math.sin(u) /g
+r = v * math.cos(u) * t
+print x, r, r * math.cos(th), x - r * math.cos(th), y - r * math.sin(th)
+
+# Then... plot everything from the pickle file...
